348432777620348e3a95c57383d5a6b71de0d643
DANIEL <alu0100951615@cc1126.etsii.ull.es>
2017-12-13 12:23:58 +0000

    Lo de Miguel

---
+++ proyecto/HandGesture.cpp
@@ -0,0 +1,91 @@
+#include "HandGesture.hpp"
+
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/video/background_segm.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+using namespace cv;
+using namespace std;
+
+HandGesture::HandGesture() {
+
+}
+
+
+double HandGesture::getAngle(Point s, Point e, Point f) {
+
+	double v1[2],v2[2];
+	v1[0] = s.x - f.x;
+	v1[1] = s.y - f.y;
+
+	v2[0] = e.x - f.x;
+	v2[1] = e.y - f.y;
+
+	double ang1 = atan2(v1[1],v1[0]);
+	double ang2 = atan2(v2[1],v2[0]);
+
+	double angle = ang1 - ang2;
+	if (angle > CV_PI) angle -= 2 * CV_PI;
+	if (angle < -CV_PI) angle += 2 * CV_PI;
+	return (angle * 180.0/CV_PI);
+}
+void HandGesture::FeaturesDetection(Mat mask, Mat output_img) {
+
+	vector<vector<Point> > contours;
+	vector<Point> contoursmax;
+	Mat temp_mask ;
+	mask.copyTo(temp_mask);//copia la mascara
+	int index = -1;
+
+        // CODIGO 3.1
+        // detección del contorno de la mano y selección del contorno más largo
+        findContours(temp_mask,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_SIMPLE);
+				for(int aux = 0; aux <contours.size(); aux++){
+					if(contours[aux].size() > contoursmax.size()){
+						contoursmax = contours[aux];
+						index = aux;
+					}
+				}
+        // pintar el contorno
+        //...
+				drawContours(output_img,contours,index,Scalar(255,0,0),2,8,vector<Vec4i>(),0,Point());
+
+	//obtener el convex hull
+	vector<int> hull;
+	convexHull(contours[index],hull);
+
+	// pintar el convex hull
+	Point pt0 = contours[index][hull[hull.size()-1]];
+	for (int i = 0; i < hull.size(); i++)
+	{
+		Point pt = contours[index][hull[i]];
+		line(output_img, pt0, pt, Scalar(0, 0, 255), 2, CV_AA);
+		pt0 = pt;
+	}
+
+        //obtener los defectos de convexidad
+	vector<Vec4i> defects;
+	convexityDefects(contours[index], hull, defects);
+
+
+		int cont = 0;
+		for (int i = 0; i < defects.size(); i++) {
+			Point s = contours[index][defects[i][0]];
+			Point e = contours[index][defects[i][1]];
+			Point f = contours[index][defects[i][2]];
+			float depth = (float)defects[i][3] / 256.0;//se divide la distancia entre 256 para que este entre 0 255
+			double angle = getAngle(s, e, f);
+                        // CODIGO 3.2
+                        // filtrar y mostrar los defectos de convexidad
+			if(depth > 50 && angle < 90){
+				circle(output_img,f,5,Scalar(0,255,0),3);
+			}
+    }
+
+
+}
